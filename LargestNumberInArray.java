6eb653a2b25b559b2be346897355897ae3d8d67b
AJAY R <63496620+ajayravi238@users.noreply.github.com>
2025-02-25 08:44:53 +0530

    Create Largest Number In Array

---
+++ LargestNumberInArray.java
@@ -0,0 +1,23 @@
+import java.util.*;
+
+public class LargestNumberInArray {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int num = sc.nextInt();
+		int[] arr = new int[num];
+		for(int i=0; i<num; i++) {
+			arr[i] = sc.nextInt();
+		}
+		for(int i=0; i<num; i++) {
+			for(int j=0; j<num-1; j++) {
+				if(arr[j] < arr[j+1]) {
+					int temp = arr[j];
+					arr[j] = arr[j+1];
+					arr[j+1] = temp;
+				}
+			}
+		}
+		System.out.println(arr[0]);
+	}
+}
