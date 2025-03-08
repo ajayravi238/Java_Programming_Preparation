53cc10e6430918b1047259062514a5be8f811e28
AJAY R <63496620+ajayravi238@users.noreply.github.com>
2025-03-08 13:04:07 +0530

    Create Move All Zero To End

---
+++ MoveAllZeroToEnd.java
@@ -0,0 +1,26 @@
+import java.util.*;
+
+public class MoveAllZeroToEnd {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int num = sc.nextInt();
+		int[] arr = new int[num];
+		for(int i=0; i<num; i++) {
+			arr[i] = sc.nextInt();
+		}
+		int k = 0;
+		int res[] = new int[num];
+		for(int i=0; i<num; i++) {
+			if(arr[i] != 0) {
+				res[k++] = arr[i];
+			}
+		}
+		for(int i=0; i<num; i++) {
+			if(arr[i] == 0) {
+				res[k++] = arr[i];
+			}
+		}
+		System.out.println(Arrays.toString(res));
+	}
+}
