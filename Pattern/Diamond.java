13eaf6247715e5fb13ee68e57731d6c25f43a342
AJAY R <63496620+ajayravi238@users.noreply.github.com>
2025-03-11 19:22:17 +0530

    Create Diamond

---
+++ Pattern/Diamond.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+
+public class Diamond {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int num = sc.nextInt();
+		for(int i=0; i<num; i++) {
+			for(int j=i; j<num-1; j++) {
+				System.out.print(" ");
+			}
+			for(int j=0; j<=i; j++) {
+				System.out.print("*");
+			}
+			for(int j=0; j<i; j++) {
+				System.out.print("*");
+			}
+			System.out.println();
+		}
+		for(int i=0; i<num; i++) {
+			for(int j=0; j<i; j++) {
+				System.out.print(" ");
+			}
+			for(int j=i; j<num; j++) {
+				System.out.print("*");
+			}
+			for(int j=i; j<num-1; j++) {
+				System.out.print("*");
+			}
+			System.out.println();
+		}
+	}
+}
