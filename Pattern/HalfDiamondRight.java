7a84d702730eb9c2f2467541d2a5485486170633
AJAY R <63496620+ajayravi238@users.noreply.github.com>
2025-03-11 19:21:39 +0530

    Create Half Diamond Right

---
+++ Pattern/HalfDiamondRight.java
@@ -0,0 +1,23 @@
+// Side Triangle Face Right
+
+import java.util.Scanner;
+
+public class HalfDiamondRight {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int num = sc.nextInt();
+		for(int i=0; i<num; i++) {
+			for(int j=0; j<=i; j++) {
+				System.out.print("*");
+			}
+			System.out.println();
+		}
+		for(int i=0; i<num; i++) {
+			for(int j=i; j<num-1; j++) {
+				System.out.print("*");
+			}
+			System.out.println();
+		}
+	}
+}
