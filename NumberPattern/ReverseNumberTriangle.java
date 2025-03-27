56d70468688e9b0dec36ced5b270c0a7769489f8
AJAY R <63496620+ajayravi238@users.noreply.github.com>
2025-03-27 10:29:18 +0530

    Create ReverseNumber Triangle

---
+++ NumberPattern/ReverseNumberTriangle.java
@@ -0,0 +1,27 @@
+// Input:
+// 5
+// Output:
+// 1 2 3 4 5 
+//  2 3 4 5 
+//   3 4 5 
+//    4 5 
+//     5 
+
+import java.util.Scanner;
+
+public class ReverseNumberTriangle {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int num = sc.nextInt();
+		for(int i=1; i<=num; i++) {
+			for(int j=1; j<i; j++) {
+				System.out.print(" ");
+			}
+			for(int j=i; j<=num; j++) {
+				System.out.print(j + " ");
+			}
+			System.out.println();
+		}
+	}
+}
